5c35ba822295fbecdfb3e1f515af255f556935de
Anas464410 <sa464410@gmail.com>
2023-07-17 22:56:56 +0530

    Updated model.json file path in main.js file

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ var base_url = window.location.origin;
 let model;
 (async function(){  
     console.log("model loading...");  
-    model = await tf.loadLayersModel("https://maneprajakta.github.io/Digit_Recognition_Web_App/models/model.json")
+    model = await tf.loadLayersModel("https://Anas464410.github.io/Digit-Recognition-WebApp/models/model.json")
     console.log("model loaded..");
 })();
 
